011af01c54385a4d51fc89f66bfb207f78ac1783
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-06-21 18:01:32 -0700

    initia

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <img src="./assets/jjp2.png" class="main-img" alt="logo">
+  </main>
+</template>
+
+<style>
+#app {
+ box-sizing: border-box;
+ margin: 0;
+ padding: 0;
+ font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.main-img {
+ width: 100%;
+ object-fit: cover;
+ opacity: 0.2;
+}
+</style>
