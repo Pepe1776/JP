92a7e601c95e1007380339e1693491fc91d354ba
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-06-22 22:04:54 -0700

    tri

--- src/App.vue
+++ src/App.vue
@@ -113,16 +113,16 @@ main {
   durations and timing functions.
 */
 .slide-fade-enter-active {
-  transition: all 0.3s ease-out;
+  transition: all 0.5s ease-out;
 }
 
 .slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 1s cubic-bezier(1, 0.9, 0.4, 2);
 }
 
 .slide-fade-enter-from,
 .slide-fade-leave-to {
-  transform: translateX(20px);
+  transform: translate(-150%, -100%) scale(0.3);
   opacity: 0;
 }
 </style>
