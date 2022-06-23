b06c7d57bceb01b3a1639e5152754e20004737c7
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-06-22 22:22:49 -0700

    ui

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const show = ref(true);
+const show2 = ref(false);
 </script>
 
 <template>
   <nav>
-    <router-link to="/">
-      <img class="brand" src="./assets/jjp2.png" alt="logo">
-    </router-link>
+
+    <img @click="show2 = !show2" class="brand" src="./assets/jjp2.png" alt="logo">
+
   </nav>
   <main>
     <Transition name="slide-fade">
-      <img v-if="show" src="./assets/011.jpeg" class="main-img" alt="logo" />
+      <img v-if="show" src="./assets/jjp2.png" class="main-img" alt="logo" />
     </Transition>
-   <button v-if="show" class="enter-btn" @click="show = !show">Enter</button>
+    <Transition name="slide-fade">
+      <img v-if="show2" src="./assets/011.jpeg" class="main-img2" alt="logo" />
+    </Transition>
+    <Transition name="slide-fade">
+      <img v-if="show2" src="./assets/011.jpeg" class="main-img" alt="logo" />
+    </Transition>
+    <button v-if="show" class="enter-btn" @click="show = !show">Enter</button>
   </main>
 </template>
 
@@ -41,6 +48,14 @@ main {
  top: 50%;
  transform: translate(-50%, -50%);
  opacity: 0.3;
+}
+.main-img2 {
+ position: absolute;
+  width: 100px;
+  height: auto;
+  left: 3%;
+  top: 3%;
+  opacity: 0.9;
 }
 .brand {
   position: absolute;
@@ -124,5 +139,17 @@ main {
 .slide-fade-leave-to {
   transform: translate(-150%, -100%) scale(0.3);
   opacity: 0;
+}
+
+.fade-slide-enter-active {
+  transition: all 0.5s cubic-bezier(0.4, 0, 0.2, 1);
+}
+.fade-slide-leave-active {
+  transition: all 1s cubic-bezier(1, 0.9, 0.4, 2);
+}
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  opacity: 1;
+  transform: translate(500%,  -50%) scale(8);
 }
 </style>
